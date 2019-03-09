0a3b94248fdffcb721941dc8932955b4cfe1daf3
cice <cice@08User06.CICE.LOCAL>
2019-03-09 14:55:36 +0100

    Testeando output-generator

---
+++ tests/unit/lib/output-generator.test.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const outputGenerator = require("../../../lib/output-generator");
+
+describe("Testing de setListeners", () => {
+    it("Debería devolver los bits que ocupa el archivo", () => {
+        input = outputStream.on;
+    })
+})
