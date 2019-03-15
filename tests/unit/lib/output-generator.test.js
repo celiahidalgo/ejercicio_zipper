2f7c8bfe0229e8f6cbc14d83511d6fcb0c25e7b6
Celia Hidalgo <Doers@MacBook-Pro-de-Celia.local>
2019-03-15 20:51:44 +0100

    testing output-generator

--- tests/unit/lib/output-generator.test.js
+++ tests/unit/lib/output-generator.test.js
@@ -1,8 +1,41 @@
+const sinon = require("sinon");
 const fs = require("fs");
 const outputGenerator = require("../../../lib/output-generator");
 
 describe("Testing de setListeners", () => {
-    it("Debería devolver los bits que ocupa el archivo", () => {
-        input = outputStream.on;
-    })
+    it("Debería llamar a la función setListeners", () => {
+      function received(){
+        outputGenerator.setListeners;
+      }
+      const spy = sinon.spy();
+      
+      received([], spy);
+      
+      expect(spy.called);
+    });
+ })
+
+
+
+describe("Testing de generateOutputStream", () => {
+    let context;
+    let streamStubbed;
+  
+    beforeEach(() => {
+      context = sinon.createSandbox();
+      streamStubbed = context.stub(fs, "createWriteStream");
+    });
+    afterEach(() => {
+        context.restore();
+      });
+    it("Debería tener una función para abrir un stream", function() {
+        expect(typeof outputGenerator.generateOutputStream === "function").toBe(true);
+      });
+    it("Debería devolver algo", function() {
+        streamStubbed.resolves(true);
+        expect(outputGenerator.generateOutputStream()).resolves.toBe(true);
+      });
+
 })
+
+
